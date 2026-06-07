44a20bacf824809f71f6b7267a6e0f07bd7e0292
esthermensah <edmworkmail@gmail.com>
2026-06-07 22:45:26 +0000

    Fix public key file reference and enhance security group configuration

--- main.tf
+++ main.tf
@@ -10,9 +10,10 @@ terraform{
 provider "aws" {
     region = "us-east-1"
 }
+
  resource "aws_key_pair" "dev_key"{
     key_name = "terraform-dev-key"
-    public_key = file("terraform.pub")
+    public_key = file("terraform-key.pub")
  }
 
 resource "aws_instance" "dev_server"{
@@ -28,17 +29,43 @@ resource "aws_instance" "dev_server"{
             echo "Hello from Terraform Web Server" > /var/www/html/index.html
             EOF
 
-}
-
-resource "aws_security_group" "dev_sg" {
-
-
-
-
-
-
 
     tags = { 
         Name = "terraform-web-server"
     }
+
 }
+
+resource "aws_security_group" "dev_sg" {
+    name = "terraform-dev-sg"
+    description = "allow SSH and HTTP traffic"
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+     ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "All outbound traffic"
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+}
+
+
+
