9e4930f84412cc7684b8d895d4169b506c3ab2a5
esthermensah <edmworkmail@gmail.com>
2026-06-07 22:36:36 +0000

    Add security group resource for web server configuration

--- main.tf
+++ main.tf
@@ -28,6 +28,15 @@ resource "aws_instance" "dev_server"{
             echo "Hello from Terraform Web Server" > /var/www/html/index.html
             EOF
 
+}
+
+resource "aws_security_group" "dev_sg" {
+
+
+
+
+
+
 
     tags = { 
         Name = "terraform-web-server"
