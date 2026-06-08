5887508f1f50b88773be7cb51f51c683c20c54f6
esthermensah <edmworkmail@gmail.com>
2026-06-08 15:35:41 +0000

    Add region variable definition to variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,13 @@ variable "instance_type" {
     description = "EC2 intance type"
     type = string
     default = "t3.micro"
-
 }
+
+
+variable "region" {
+    description = "AWS region"
+    type = string
+    default = "us-east-1"
+}
+
 
