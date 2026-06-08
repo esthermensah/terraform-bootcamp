174ca535437856b799d457e1607f4c91773a79b0
esthermensah <edmworkmail@gmail.com>
2026-06-08 12:08:53 +0000

    Refactor aws_instance resource to consolidate key_name and security_group_ids configuration

--- main.tf
+++ main.tf
@@ -19,7 +19,11 @@ provider "aws" {
 resource "aws_instance" "dev_server"{
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t3.micro"
-
+    
+    key_name = aws_key_pair.dev_key.key_name
+    vpc_security_group_ids = [
+        aws_security_group.dev_sg.id
+        ]
     user_data = <<-EOF
             #!/bin/bash
             yum update -y
@@ -29,10 +33,7 @@ resource "aws_instance" "dev_server"{
             echo "Hello from Terraform Web Server" > /var/www/html/index.html
             EOF
 
-    key_name = aws_key_pair.dev_key.key_name
-    vpc_security_group_ids = [
-        aws_security_group.dev_sg.id
-        ]
+
 
     tags = { 
         Name = "terraform-web-server"
