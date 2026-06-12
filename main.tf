f5e7b6c832f9188f5df4eb97c15eea044e0f5271
esthermensah <edmworkmail@gmail.com>
2026-06-12 16:45:19 +0000

    Add IAM instance profile to EC2 instance resource

--- main.tf
+++ main.tf
@@ -18,12 +18,15 @@ provider "aws" {
 
 resource "aws_instance" "dev_server"{
     ami = "ami-05ffe3c48a9991133"
+
+terraform validate
     instance_type = var.instance_type
 
     subnet_id = aws_subnet.public_a.id
 
 
     key_name = aws_key_pair.dev_key.key_name
+    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
     vpc_security_group_ids = [
         aws_security_group.dev_sg.id
         ]
